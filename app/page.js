83daed45b5a2723d0e35f7f7c7b98e09e8364c77
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-03-25 11:48:55 +0600

    connected git github

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Home',
+  description: 'ShopCenter home page description',
+};
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
