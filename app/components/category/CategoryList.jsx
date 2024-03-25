ecc52ec77b20adcce79edd4736fdfcca5ee7eee1
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-03-25 19:55:45 +0600

    changes

--- app/components/category/CategoryList.jsx
+++ app/components/category/CategoryList.jsx
@@ -12,17 +12,17 @@ const CategoryList = () => {
   ];
 
   return (
-    <div className="flex items-center justify-between w-full my-10 lg:block lg:w-2/12 lg:my-0 lg:mt-4">
+    <div className="flex flex-col items-center justify-between w-full my-10 sm:flex-row lg:block lg:w-2/12 lg:my-0 lg:mt-4">
       <Link
         href={`/category/all-products`}
         scroll={false}
         className={`${
           pathname === `/category/all-products`
-            ? 'text-[#00D991] font-semibold'
+            ? 'text-[#00D991] font-bold transition-all'
             : ''
         }`}
       >
-        <button className="box-border block h-6 mt-4 border-black hover:border-b">
+        <button className="box-border block h-6 mt-4 hover:border-b hover:border-[#00D991]">
           All
         </button>
       </Link>
@@ -34,11 +34,11 @@ const CategoryList = () => {
           key={category}
           className={`${
             pathname === `/category/${category}`
-              ? 'text-[#00D991] font-semibold'
+              ? 'text-[#00D991] font-bold transition-all'
               : ''
           }`}
         >
-          <button className="box-border block h-6 mt-5 capitalize border-black hover:border-b">
+          <button className="box-border block h-6 mt-5 capitalize hover:border-b hover:border-[#00D991]">
             {category}
           </button>
         </Link>
