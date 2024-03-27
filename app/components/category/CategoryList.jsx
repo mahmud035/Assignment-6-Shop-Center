67aadd82a2cace1b8c0f0f79659284611cf42023
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-03-27 15:06:02 +0600

    modify product category list style

--- app/components/category/CategoryList.jsx
+++ app/components/category/CategoryList.jsx
@@ -11,16 +11,16 @@ const CategoryList = () => {
   return (
     <div className="flex flex-col items-center justify-between w-full gap-6 my-10 sm:flex-row lg:block lg:w-2/12 lg:my-0 lg:mt-4">
       <button
-        className={`box-border block w-4/5 h-10 pl-2 mt-4 text-center bg-gray-200 rounded cursor-pointer sm:text-left hover:bg-emerald-100 ${
-          pathname === `/category/all-products`
-            ? 'bg-emerald-400 font-bold text-white hover:bg-emerald-400'
-            : ''
-        }`}
+        className={`box-border block w-4/5 h-10 mt-4 text-center bg-gray-200 rounded cursor-pointer sm:text-left hover:bg-emerald-100 `}
       >
         <Link
           href={`/category/all-products`}
           scroll={false}
-          className="block w-full"
+          className={`block w-full h-full rounded pl-2 pt-2 ${
+            pathname === `/category/all-products`
+              ? 'bg-emerald-400 font-bold text-white hover:bg-emerald-400'
+              : ''
+          }`}
         >
           All
         </Link>
@@ -29,16 +29,16 @@ const CategoryList = () => {
       {uniqueCategories.map((category) => (
         <button
           key={category}
-          className={`box-border block w-4/5 h-10 pl-2 mt-5 text-center capitalize bg-gray-200 rounded sm:text-left hover:bg-emerald-100 ${
-            pathname === `/category/${category}`
-              ? 'bg-emerald-400 font-bold text-white hover:bg-emerald-400'
-              : ''
-          }`}
+          className={`box-border block w-4/5 h-10 mt-5 text-center capitalize bg-gray-200 rounded sm:text-left hover:bg-emerald-100 `}
         >
           <Link
             href={`/category/${category}`}
             scroll={false}
-            className="block w-full"
+            className={`block w-full h-full rounded pl-2 pt-2 ${
+              pathname === `/category/${category}`
+                ? 'bg-emerald-400 font-bold text-white hover:bg-emerald-400'
+                : ''
+            }`}
           >
             {category}
           </Link>
